e23f8873ad8dc843767247459b07c7ef33abd8f3
Polya-Sh <shinkaryova.polina@yandex.ru>
2026-03-24 12:51:41 +0300

    начало, структура проекта

---
+++ src/core/DataProcessor.java
@@ -0,0 +1,16 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataProcessor {
+    /**
+     * ВАРИАНТ 6: Возвращает элементы первого списка,
+     * которых нет во втором списке.
+     * ({ 4, 5, 2, 1, 7, 5, 2 }, { 5, 1, 8, 9 }) → { 4, 2, 7, 2 }
+     */
+    public static List<Integer> processVariant6(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
+}
